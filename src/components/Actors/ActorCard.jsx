8093bec55426660edaa991886e87e12180039d58
Mohit <mohitx618@gmail.com>
2024-02-07 14:43:31 +0530

    Updates

--- src/components/Actors/ActorCard.jsx
+++
@@ -1,5 +0,0 @@
-const ActorCard = () => {
-  return <div>ActorCard</div>;
-};
-
-export default ActorCard;
--- src/components/Actors/Actors.jsx
+++
@@ -1,6 +0,0 @@
-const Actors = () => {
-  console.log("bina baat mujhe phekre liya hai yeh bidu dekh le tu kahi mein kuch kar na du")
-  return <div>Actors</div>;
-};
-
-export default Actors;
